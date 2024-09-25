f5a2036e7fdee0e94a6b0da3af65c6a56d486626
Santosh Thapa Magar <santosh.magar@yipl.com.np>
2024-09-25 21:08:00 +0545

    version 1.0.0

--- version.php
+++ version.php
@@ -36,7 +36,7 @@ $plugin->component = 'local_easycustmenu';
 $plugin->release = '1.0.0';
 
 // This is the version of the plugin.
-$plugin->version = 2024092300;
+$plugin->version = 2024092500;
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
