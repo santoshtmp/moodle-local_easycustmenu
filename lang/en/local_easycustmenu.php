df92b6e21418e5ddc3bc54ec46de31453feb09d9
Santosh Thapa Magar <santosh.magar@yipl.com.np>
2024-10-23 21:24:37 +0545

    update

--- lang/en/local_easycustmenu.php
+++ lang/en/local_easycustmenu.php
@@ -62,3 +62,5 @@ $string['show_the_menu_when_user_is'] = "Show the menu when user is";
 $string['show_the_menu_when_language_is'] = "Show the menu when language is";
 $string['open_in_a_new_browser_window'] = "Open in a new browser window : ";
 $string['show_the_menu_when_language_placeholder'] = "language code Example: en, es, ne";
+
+$string['privacy:metadata'] = 'local easycustmenu plugin is to manage the site menu item orders and does not store any data itself.';
