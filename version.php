3178140f9465bf6084dce178aaa8ad25eac8a061
Santosh Thapa Magar <45563917+santoshtmp@users.noreply.github.com>
2025-08-17 15:22:10 +0545

    Update version.php

--- version.php
+++ version.php
@@ -34,7 +34,7 @@ $plugin->component = 'local_easycustmenu';
 $plugin->release = '2.0.0';
 
 // This is the version of the plugin.
-$plugin->version = 2025081701;
+$plugin->version = 2025081703;
 
 // This is a stable release.
 $plugin->maturity = MATURITY_STABLE;
