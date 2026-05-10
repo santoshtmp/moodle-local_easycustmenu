434ddd946ed605f70addb15125b1f0b5c1e958ca
Santosh Thapa Magar <45563917+santoshtmp@users.noreply.github.com>
2026-05-10 22:14:26 +0545

    Fix formatting and add newline at end of file

--- classes/hooks/hook_callbacks.php
+++ classes/hooks/hook_callbacks.php
@@ -38,7 +38,6 @@ use local_easycustmenu\helper;
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class hook_callbacks {
-
     /**
      * Callback to modify primary navigation.
      *
